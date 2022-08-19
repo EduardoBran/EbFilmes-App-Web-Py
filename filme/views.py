0de437baa09453eef83361503b687ae3d69fd25d
Eduardo Brandao <eduardo.ads1814@gmail.com>
2022-08-19 12:55:44 -0300

    exibindo os filmes cadastrados

--- filme/views.py
+++ filme/views.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.views.generic.list import ListView
+
+from .models import *
 
 
-class FilmeIndex(TemplateView):
+class FilmeIndex(ListView):
+    model = Filme
     template_name = 'filme/index.html'
+    context_object_name = 'allMovies'
+    
+    def get_queryset(self):
+        qs = super().get_queryset()
+        qs = qs.order_by('nome')
+        return qs
