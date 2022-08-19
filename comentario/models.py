b28bbfc929eb50fcd2eb0f9902cefb6f8da10bfd
Eduardo Brandao <eduardo.ads1814@gmail.com>
2022-08-19 12:32:15 -0300

    add models para filme | categoria | comentario

--- comentario/models.py
+++ comentario/models.py
@@ -1,3 +1,13 @@
+from django.contrib.auth.models import User
 from django.db import models
+from filme.models import Filme
 
-# Create your models here.
+
+class Comentario(models.Model):
+    filme = models.ForeignKey(Filme, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='Usuário')
+    comentario = models.TextField(max_length=1000, verbose_name='Comentário')
+    nota = models.FloatField(default=0)
+    
+    def __str__(self):
+        return self.user.username
--- filme/models.py
+++ filme/models.py
@@ -1,3 +1,20 @@
+from categoria.models import Categoria
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class Filme(models.Model):
+    nome = models.CharField(max_length=50)
+    diretor = models.CharField(max_length=100)
+    elenco = models.CharField(max_length=200)
+    sinopse = models.TextField(max_length=800)
+    data_estreia = models.DateField(default=timezone.now, verbose_name='Data lançamento')
+    nota_media = models.FloatField(default=0, verbose_name='Nota Média')
+    categoria_filme = models.ForeignKey(Categoria, on_delete=models.DO_NOTHING,  blank=True, null=True, verbose_name='Categoria') 
+    imagem_filme = models.URLField(default=None, null=True)
+    
+    def __str__(self):
+        return self.nome
+    
+    def __unicode__(self):
+        return self.nome
