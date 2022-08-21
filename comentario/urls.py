dc5917195f8fb01485d66305478baaf7ba75a1bd
Eduardo Brandao <eduardo.ads1814@gmail.com>
2022-08-21 12:45:19 -0300

    add opcao de editar comentario somente com o usuario que o registrou

--- comentario/urls.py
+++ comentario/urls.py
@@ -5,5 +5,7 @@ from . import views
 app_name = 'comentario'
 
 urlpatterns = [
-    path('addComentario/<int:movie_id>/', views.addComentario, name='addComentario')
+    path('addComentario/<int:movie_id>/', views.addComentario, name='addComentario'),
+    path('editComentario/<int:movie_id>/<int:comentario_id>', views.editComentario, name='editComentario'),
+    
 ]
--- comentario/views.py
+++ comentario/views.py
@@ -28,3 +28,26 @@ def addComentario(request, movie_id):
         return render(request, 'filme/filme_detalhes.html', context)
     else:
         return redirect('perfil:login')    
+
+
+def editComentario(request, movie_id, comentario_id):
+    if request.user.is_authenticated:
+        comentario = Comentario.objects.get(filme=movie_id, pk=comentario_id)
+        
+        if request.user == comentario.user:
+            if request.method == 'POST':
+                form = ComentarioForm(request.POST or None, instance=comentario)
+                
+                if form.is_valid():
+                    data = form.save(commit=False)
+                    data.save()
+                    return redirect('filmes:detail', movie_id)
+            else:
+                form = ComentarioForm(instance=comentario)
+            
+            context = {'form': form}
+            return render(request, 'comentario/editComentario.html', context)
+        else:
+            return redirect('filmes:detail', movie_id)
+    else:                
+        return redirect('perfil:login')
