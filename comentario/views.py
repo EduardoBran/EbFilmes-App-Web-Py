8d3b352cd90ebe77dacd14d88c06dddb102c9de1
Eduardo Brandao <eduardo.ads1814@gmail.com>
2022-08-22 10:58:53 -0300

    mudando nome variavel em editComentario

--- comentario/views.py
+++ comentario/views.py
@@ -32,18 +32,18 @@ def addComentario(request, movie_id):
 
 def editComentario(request, movie_id, comentario_id):
     if request.user.is_authenticated:
-        comentario = Comentario.objects.get(filme=movie_id, pk=comentario_id)
+        user_comentario = Comentario.objects.get(filme=movie_id, pk=comentario_id)
         
-        if request.user == comentario.user:
+        if request.user == user_comentario.user:
             if request.method == 'POST':
-                form = ComentarioForm(request.POST or None, instance=comentario)
+                form = ComentarioForm(request.POST or None, instance=user_comentario)
                 
                 if form.is_valid():
                     data = form.save(commit=False)
                     data.save()
                     return redirect('filmes:detail', movie_id)
             else:
-                form = ComentarioForm(instance=comentario)
+                form = ComentarioForm(instance=user_comentario)
             
             context = {'form': form}
             return render(request, 'comentario/editComentario.html', context)
