aea7bc0a85cbefd27b8eee8ffdb2d8db298336c4
Eduardo Brandao <eduardo.ads1814@gmail.com>
2022-08-22 20:13:26 -0300

    edit layout card movie

--- filme/views.py
+++ filme/views.py
@@ -11,7 +11,7 @@ class FilmeIndex(ListView):
     model = Filme
     template_name = 'filme/index.html'
     context_object_name = 'allMovies'
-    paginate_by = 2
+    paginate_by = 8
     
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
