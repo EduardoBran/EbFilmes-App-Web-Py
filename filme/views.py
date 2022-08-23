1bc09f7d8b7c9ecbad1f8b9049568ccbb93a659b
Eduardo Brandao <eduardo.ads1814@gmail.com>
2022-08-23 11:09:38 -0300

    edit page filme_busca para exibir o que foi buscado

--- filme/views.py
+++ filme/views.py
@@ -59,6 +59,11 @@ class FilmeCategoria(FilmeIndex):
 class FilmeBusca(FilmeIndex):
     template_name = 'filme/filme_busca.html'
     
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['termo'] = self.request.GET.get('termo')
+        return context
+    
     def get_queryset(self):
         qs = super().get_queryset()
         termo = self.request.GET.get('termo')
