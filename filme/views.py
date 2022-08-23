0f8903563781d18cc14a1c34a5e953f3e2a2f521
Eduardo Brandao <eduardo.ads1814@gmail.com>
2022-08-23 12:58:12 -0300

    exbindo qual categoria foi pesquisada em filmes_categoria

--- filme/views.py
+++ filme/views.py
@@ -18,6 +18,7 @@ class FilmeIndex(ListView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['categorias'] = Categoria.objects.all()
+        context['categoria'] = self.kwargs.get('categoria', None)
         context['termo'] = self.request.GET.get('termo')
         return context
     
